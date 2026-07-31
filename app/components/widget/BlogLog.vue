85a9d9fcf0cbe6bcf2560d4459178b286c0d325a
xilele777 <2519175810@qq.com>
2026-07-31 19:57:02 +0800

    update blog changelog

--- app/components/widget/BlogLog.vue
+++ app/components/widget/BlogLog.vue
@@ -4,6 +4,7 @@ const { timeEstablished } = useAppConfig()
 const blogLog = [
 	{ label: '2026-03-16', value: '博客搭建完成' },
 	{ label: '2026-05-16', value: '博客功能完善' },
+	{ label: '2026-07-31', value: '新增后台' },
 ]
 </script>
 
