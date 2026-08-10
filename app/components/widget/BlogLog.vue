302fdbb7513e75dc49335bd4ecd3363255be9596
xiaoguoba <2519175810@qq.com>
2026-08-10 20:37:28 +0800

    update blog: 1 staged change

--- app/components/widget/BlogLog.vue
+++ app/components/widget/BlogLog.vue
@@ -4,6 +4,7 @@ const blogLog = [
 	{ label: '2026-03-16', value: '博客搭建完成' },
 	{ label: '2026-05-16', value: '博客功能完善' },
 	{ label: '2026-07-31', value: '新增后台' },
+	{ label: '2026-08-10', value: '新增Wiki站入口' },
 ]
 // BLOG_ADMIN_LOG_END
 </script>
