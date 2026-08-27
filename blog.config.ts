9014071667981d85b8b764e40cd5f94a1efb2eb8
xilele777 <2519175810@qq.com>
2026-08-27 11:29:46 +0800

    chore: update site avatar and favicon

--- blog.config.ts
+++ blog.config.ts
@@ -7,7 +7,7 @@ const basicConfig = {
 	description: '这是我的个人博客，用于记录自己的学习与生活。在非个人干涉的情况下，博客具有接近匿名的特性，因此我把它当作一个私人的记事本。',
 	author: {
 		name: '小锅巴',
-		avatar: '/avatar.jpg',
+		avatar: '/Gemini_Generated_Image_wizyuzwizyuzwizy.jpg',
 		email: 'xiaoguoba6@gmail.com',
 		homepage: 'https://github.com/xilele777',
 	},
@@ -16,7 +16,7 @@ const basicConfig = {
 		name: '署名-非商业性使用-相同方式共享 4.0 国际',
 		url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans',
 	},
-	favicon: '/favicon.png',
+	favicon: '/Gemini_Generated_Image_wizyuzwizyuzwizy.jpg',
 	language: 'zh-CN',
 	timeEstablished: '2026-03-16 21:00:00',
 	timeZone: 'Asia/Shanghai',
